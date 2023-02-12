f0298852d4f655c04798c8ffc0d2a5db17f0019d
romanshusterman <romanshusterman@gmail.com>
2023-02-12 10:27:26 +0200

    Update celery_settings.py

--- celery_settings.py
+++ celery_settings.py
@@ -1,4 +1,4 @@
 import os
 CELERY_TASK_SERIALIZER = 'json'
-BROKER_URL = os.environ.get('REDIS_URL','redis://localhost:6379/0')
+BROKER_URL = os.environ.get('REDIS_URL')
 CELERY_ACCEPT_CONTENT = ['json']
